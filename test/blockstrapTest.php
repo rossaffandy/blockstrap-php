670b5b25b28069837498e218a5796815ef7edc45
Ross Affandy <affandy@planyourwedding.my>
2015-07-20 11:13:51 +0800

    Updating unit test

--- test/blockstrapTest.php
+++ test/blockstrapTest.php
@@ -1,4 +1,5 @@
 <?php
+include_once '../modules/blockstrap.php';
 
 /**
  * User: Affandy
@@ -8,4 +9,45 @@
 class blockstrapTest extends \PHPUnit_Framework_TestCase
 {
 
+    private $key, $value;
+
+    protected function setUp()
+    {
+        $_POST = array();
+        $_GET = array();
+
+        $this->key = 'test';
+        $this->value = 'testing';
+    }
+
+    public function tearDown()
+    {
+
+    }
+
+    public function testGetVarPost() {
+        $_POST = array($this->key => $this->value);
+
+        $bs = new blockstrap();
+        $result = $bs->get_var($this->key);
+
+        $this->assertEquals($this->value, $result);
+    }
+
+    public function testGetVarGet() {
+        $_GET = array($this->key => $this->value);
+
+        $bs = new blockstrap();
+        $result = $bs->get_var($this->key);
+
+        $this->assertEquals($this->value, $result);
+    }
+
+    public function testGetVarFailure() {
+
+        $bs = new blockstrap();
+        $result = $bs->get_var($this->key);
+
+        $this->assertFalse($result);
+    }
 }
